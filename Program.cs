1383b939dd8a6808549331a7f7e9a107e5017830
ZimeerVir <sanya.viryasov.00.00@mail.ru>
2021-09-18 20:00:57 +0300

    Exercise_1
    Я не уверен, что я все сделал, как надо, но я старался)

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Davai_posmotrim
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Добро пожаловать в программу по высчитыванию D и L из S.");
+            Console.WriteLine();    
+            Console.Write("Пожалуйста, введите площадь круга: ");
+
+            double S = double.Parse(Console.ReadLine());
+
+            double D = Math.Round(Math.Sqrt((4 * S) / 3.14), 3);
+            double L = Math.Round(3.14 * D, 3);
+
+            Console.WriteLine();
+            Console.WriteLine("Произведя наиточнейшие расчеты, мы получили:");
+            Console.WriteLine("     Диаметр данной окружности равен: " + D);
+            Console.Write("     Длина данной окружности равена: " + L);
+            string a = Console.ReadLine();
+        }
+    }
+}
